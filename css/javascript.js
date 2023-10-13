5453402838bf7cac3b585ba455b1648d17d783f2
SANKET NIMBARGI <sanketnimbargi150@gmail.com>
2023-10-13 09:53:05 +0530

    first commit

---
+++ css/javascript.js
@@ -0,0 +1,17 @@
+// math.floor()
+// Math.ceil()
+// math.random()
+// math.random  *3
+
+// variables  : (var,const,let)
+
+// === triple equal checks equality and data type .
+
+// String Interpolation:=
+// Referencing a variable within a string.
+// In this case, ${name} will be replaced with the value
+//  of name.
+
+// arrow function modern way to use function.
+
+
